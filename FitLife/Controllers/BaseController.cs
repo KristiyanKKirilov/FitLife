331ec05788a07a19818dcd97c6ab60815e7e915f
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-04-10 21:36:27 +0300

    BaseController created

---
+++ FitLife/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitLife.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {     
+       
+    }
+}
