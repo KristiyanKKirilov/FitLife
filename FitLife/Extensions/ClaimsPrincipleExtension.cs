dfd1c625556f63af1d51fc0b47d3835934d428d4
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-04-10 23:28:30 +0300

    GlobalConstants and ViewModels class libraries added

---
+++ FitLife/Extensions/ClaimsPrincipleExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Security.Claims
+{
+    public static class ClaimsPrincipleExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static string Email(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.Email);
+        }
+    }
+}
