06b9f6a399d7ec4f42eeb465a7c084a967ea1437
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-04-13 18:53:08 +0300

    All Training Programs action and view completed

---
+++ FitLife.Web.ViewModels/TrainingProgram/AllTrainingProgramsQueryModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitLife.Web.ViewModels.TrainingProgram
+{
+    public class AllTrainingProgramsQueryModel
+    {
+        public AllTrainingProgramsQueryModel()
+        {
+            TrainingPrograms = new List<TrainingProgramServiceModel>();
+        }
+
+        public int TrainingProgramsPerPage { get; } = 6;       
+
+        public int CurrentPage { get; init; } = 1;
+
+        public int TotalTrainingProgramsCount { get; set; }       
+
+        public IEnumerable<TrainingProgramServiceModel> TrainingPrograms { get; set; }
+       
+    }
+}
---
+++ FitLife.Web.ViewModels/TrainingProgram/TrainingProgramServiceModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static FitLife.GlobalConstants.DataConstants;
+using static FitLife.GlobalConstants.ErrorMessages;
+
+namespace FitLife.Web.ViewModels.TrainingProgram
+{
+    public class TrainingProgramServiceModel
+    {
+        [Key]
+        public string Id { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        [StringLength(TitleMaxLength,
+             MinimumLength = TitleMinLength,
+            ErrorMessage = StringLengthError)]
+        public string Title { get; set; } = null!;       
+
+        [Required(ErrorMessage = RequiredError)]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredError)]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = RequiredError)]
+        public int Duration { get; set; }
+
+        [Required(ErrorMessage = RequiredError)]
+        [Display(Name = "Category")]
+        public string CategoryName { get; set; }       
+
+        [Required(ErrorMessage = RequiredError)]
+        [Display(Name = "Trainer")]
+        public string CreatorName { get; set; } = null!;    
+
+    }
+}
