9d3fff95655cb0aaaf65a9d1077cf7666d167c9d
Kristiyan Kirilov <kirilov1905@gmail.com>
2024-04-21 18:17:59 +0300

    Admin Mine TrainingProgram Action and View implemented

--- FitLife/Areas/Admin/Controllers/HomeController.cs
+++ FitLife/Areas/Admin/Controllers/HomeController.cs
@@ -1,37 +1,48 @@
 ﻿using FitLife.Core.Contracts;
+using FitLife.Core.Services;
 using FitLife.Web.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace FitLife.Web.Areas.Admin.Controllers
 {
     public class HomeController : AdminBaseController
-	{
-		private readonly IProductService productService;
+    {
+        private readonly IProductService productService;
+        private readonly ITrainerService trainerService;
+        private readonly ITrainingProgramService trainingProgramService;
 
-		public HomeController(IProductService _productService)
-		{
-			productService = _productService;
-		}
 
-		public IActionResult DashBoard()
-		{
-			return View();
-		}
+        public HomeController(IProductService _productService,
+            ITrainerService _trainerService,
+            ITrainingProgramService _trainingProgramService)
+        {
+            productService = _productService;
+            trainerService = _trainerService;
+            trainingProgramService = _trainingProgramService;
+        }
 
-		public async Task<IActionResult> ProductOptions()
-		{
-			var model = new ProductOptionsModel
-			{
-				Products = await productService
-				.AllAsync()
-			};
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
 
-			return View(model);			
+        public async Task<IActionResult> ProductOptions()
+        {
+            var model = new ProductOptionsModel
+            {
+                Products = await productService
+                .AllAsync()
+            };
 
-		}
+            return View(model);
 
-		
-		
-		
-	}
+        }
+
+        
+
+
+
+
+    }
 }
